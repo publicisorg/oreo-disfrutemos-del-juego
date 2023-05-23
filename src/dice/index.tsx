66f669127a03db18a994ed847bacee207f52b138
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-05-23 19:20:08 -0300

    cambio dado y configuracion

--- src/dice/index.tsx
+++ src/dice/index.tsx
@@ -1,19 +1,31 @@
 import DiceBox from "@3d-dice/dice-box";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Dice() {
+
+    const [isDiceInit, setDiceInit] = useState(false);
 
     useEffect(() => {
         const diceBox = new DiceBox("#dice-box", {
             assetPath: "/assets/dice-box/", // required
             theme: "default",
+            themeColor: '#e7e6ca',
             offscreen: true,
-            scale: 6
+            scale: 4,
+            mass: 0.9,
+            friction: 0.8,
+            throwForce: 10,
+            startingHeight: 10,
+            restitution: 0.75,
         });
 
-        diceBox.init().then(() => {
-            diceBox.roll("1d6");
-        });
+        if (!isDiceInit) {
+            diceBox.init().then(() => {
+                setDiceInit(true);
+                diceBox.roll("1d6");
+            });
+        }
+
     }, [])
 
 

084da80e5b241d137942a449e1fac9fe44cd90ad
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-05-24 17:07:28 -0300

    cambios cubo

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ function App() {
     <main className="bg-[#00449B] w-full h-screen flex justify-center items-center">
       <section className='bg-[#0054BB] min-h-[600px] w-full h-screen lg:w-1/4 lg:h-[90%] lg:border-2 lg:rounded-3xl drop-shadow-lg relative'>
         <div className="z-50 absolute left-0 top-0 w-full h-full flex flex-col justify-center items-center">
-          
+
         </div>
         <div className="z-0 absolute left-0 top-0 w-full h-full flex flex-col justify-center items-center">
-          <Dice/>
+          <div className="bg-white h-1/2 w-full">
+            sarasa
+          </div>
+          <Dice />
         </div>
       </section>
     </main>
--- src/dice/index.tsx
+++ src/dice/index.tsx
@@ -3,34 +3,29 @@ import { useEffect, useState } from "react";
 
 function Dice() {
 
-    const [isDiceInit, setDiceInit] = useState(false);
-
     useEffect(() => {
         const diceBox = new DiceBox("#dice-box", {
             assetPath: "/assets/dice-box/", // required
             theme: "default",
-            themeColor: '#e7e6ca',
+            themeColor: '#EEEEEE',
             offscreen: true,
-            scale: 4,
+            scale: 8,
             mass: 0.9,
             friction: 0.8,
             throwForce: 10,
             startingHeight: 10,
             restitution: 0.75,
         });
-
-        if (!isDiceInit) {
-            diceBox.init().then(() => {
-                setDiceInit(true);
-                diceBox.roll("1d6");
-            });
-        }
+        
+        diceBox.init().then(() => {
+            diceBox.roll("1d6");
+            diceBox.onRollComplete = (rollResult) => console.log(rollResult[0].value)
+        });
 
     }, [])
 
-
     return (
-        <div id="dice-box" className="w-full h-full relative box-border">
+        <div id="dice-box" className="w-full h-1/2 relative box-border">
 
         </div>
     )

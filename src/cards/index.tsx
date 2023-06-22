8811ba4b5767876a2a19465dc7d69a1b46058eeb
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-06-22 13:32:31 -0300

    fondo cards

--- src/cards/index.tsx
+++ src/cards/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useSprings, animated, to as interpolate } from '@react-spring/web'
 import styles from './styles.module.css'
 import preguntas from './cards.json'
-import { useGesture } from 'react-use-gesture';
+import CardsBackground from './background';
 
 const to = (i) => ({
   x: 0,
@@ -113,6 +113,7 @@ function Deck(props: any) {
 
   return (
     <>
+      <CardsBackground />
       {!props.tutorial && propsCards.map(({ x, y, rot, scale }, i) => (
         <animated.div className={styles.deck} key={i} style={{ x, y }}>
           <animated.div style={{ transform: interpolate([rot, scale], trans) }} className="text-center rounded-md p-[2px]">
@@ -149,121 +150,7 @@ function Deck(props: any) {
 export default function Cards(props: any) {
   return (
     <div className={styles.container}>
-      {true && <Deck tutorial={props.tutorial} changeCard={props.changeCard} setCardsCount={props.setCardsCount} countCards={props.countCards} />}
-      {false && <DeckNew tutorial={props.tutorial} changeCard={props.changeCard} setCardsCount={props.setCardsCount} countCards={props.countCards} />}
+      <Deck tutorial={props.tutorial} changeCard={props.changeCard} setCardsCount={props.setCardsCount} countCards={props.countCards} />
     </div>
   );
 }
-
-//#######################
-
-
-export function DeckNew(props: any) {
-
-  const styleStatic = "bg-red-500";
-  const styleMoving = "";
-  const styleFalling = "";
-
-  const [actualCards, setActualCards] = useState<any>([]);
-  const [cardsID, setCardsID] = useState<any>([]);
-  const [styles, setStyles] = useState<any>([]);
-
-  useEffect(() => {
-
-    const auxStyles:any = [];
-    for (let i = 0; i < 100; i++) {
-      auxStyles.push(styleStatic);
-    }
-    setStyles(auxStyles);
-
-    setActualCards([
-      {
-        titulo: "PASA A LA SIGUIENTE CARTA",
-        descripcion: "Apretando el botón “siguiente” descubrís diferentes consignas para divertirte en familia.",
-        pregunta: "Pregunta",
-        index: 3,
-        id: 3,
-        categoria: "Categoria"
-      },
-      {
-        titulo: "TIRÁ EL DADO",
-        descripcion: "El color del dado determina qué pasa en el juego.",
-        pregunta: "Pregunta",
-        index: 2,
-        id: 2,
-        categoria: "Categoria"
-      },
-      {
-        titulo: "SE JUEGA POR TURNO",
-        descripcion: "Cuando veas la carta en pantalla, podés empezar.",
-        pregunta: "Pregunta",
-        index: 1,
-        id: 1,
-        categoria: "Categoria"
-      }
-    ]);
-  }, [])
-
-  useEffect(() => {
-    if (!props.tutorial) {
-      setActualCards(
-        [
-          preguntas[Math.floor(Math.random() * 100)],
-          preguntas[Math.floor(Math.random() * 100)],
-          preguntas[Math.floor(Math.random() * 100)]
-        ]
-      )
-    }
-  }, [props.tutorial])
-
-  useEffect(() => {
-    if (actualCards?.length > 0) {
-      setCardsID([
-        {
-          id: actualCards[0].id
-        },
-        {
-          id: actualCards[1].id
-        },
-        {
-          id: actualCards[2].id
-        }
-      ]);
-    }
-  }, [actualCards])
-
-  return (
-    <>
-      {!props.tutorial && actualCards.map((card, i: number) => (
-        <div className={`${styles.deck} absolute`} key={i}>
-          <div id={card.id} className={`${styles[card.id]} text-center rounded-md p-[2px] duration-500`}>
-            <p className="bg-[#0054BA] text-white rounded-t-md font-regular text-[12px] py-1 uppercase">
-              {card.categoria}
-            </p>
-            <p className="text-[16px] text-[#0054BA] justify-center items-center font-bold pluto-black mx-auto w-11/12 h-[85%] flex uppercase">
-              {card.pregunta}
-            </p>
-          </div>
-        </div>
-      ))}
-      {props.tutorial && actualCards.map((card, i: number) => (
-        <div className={styles.deck} key={i}>
-          <div id={card.id} className={`${styles[card.id]} flex justify-center items-center flex-col rounded-md p-[2px] duration-300`}>
-            <p className="text-[20px] text-[#0054BA] pluto-black justify-center items-center font-bold pluto-black w-5/6 mx-auto text-center">
-              {card.titulo}
-            </p>
-            <p className="text-[12px] text-[#0054BA] pluto justify-center items-center pluto-black w-5/6 mx-auto text-center">
-              {card.descripcion}
-            </p>
-            <div className='flex flex-row justify-center items-center w-full gap-1 absolute bottom-2'>
-              <div className={`rounded-full aspect-square h-3 ${card.index == 1 ? "border-[#0054BA] bg-[#0054BA]" : "border-[#D9D9D9] bg-transparent"} border-2`} />
-              <div className={`rounded-full aspect-square h-3 ${card.index == 2 ? "border-[#0054BA] bg-[#0054BA]" : "border-[#D9D9D9] bg-transparent"} border-2`} />
-              <div className={`rounded-full aspect-square h-3 ${card.index == 3 ? "border-[#0054BA] bg-[#0054BA]" : "border-[#D9D9D9] bg-transparent"} border-2`} />
-            </div>
-          </div>
-        </div>
-      ))}
-    </>
-  );
-}
-

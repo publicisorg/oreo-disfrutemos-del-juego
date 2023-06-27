7478dc438c15398ba87f2f591d2dbeac921cff80
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-06-27 14:56:56 -0300

    correccion boton

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -60,7 +60,7 @@ function UI(props: any) {
         <>
             <div className="h-1/3 w-full top-0 absolute" onClick={() => handleNextTutorial()} />
             <div className="h-2/3 w-full bottom-0 absolute" onClick={() => handleFlyOut()} />
-            <div className={`${showMessage} absolute bottom-[4.5rem] w-full flex justify-center items-center duration-300`}>
+            <div className={`${showMessage} absolute bottom-[5.2rem] w-full flex justify-center items-center duration-300`}>
                 <div className={`max-w-[300px] text-white text-[10px] uppercase duration-300 h-[45px] px-8 pt-1 rounded-full w-[260px] text-center flex justify-center items-center`} style={{ backgroundColor: diceResultColor }}>
                     {colors[props.diceResult].text}
                 </div>

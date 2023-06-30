ea625849bb48587ae2564fd12017b85f3add91d6
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-06-30 17:07:50 -0300

    menu principal mas chico

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -29,11 +29,11 @@ function Home(props: any) {
 
     return (
         <div className="relative mx-autoh-screen w-full h-full">
-            <div className={`w-full h-full inset-0 flex justify-center items-center text-white text-6xl md:text-7xl lg:text-8xl xl:text-9xl relative duration-300 ${opacity}`}>
+            <div className={`w-full h-full inset-0 flex justify-center items-center text-white text-6xl md:text-7xl lg:text-8xl relative duration-300 ${opacity}`}>
                 <div className={`relative flex justify-center items-center ${rotation} ${scale} ${duration} mb-[75px] lg:mb-[15px]`}>
-                    <h1 className={`pluto-black text-center tracking-tighter leading-[50px] md:leading-[56px] lg:leading-[75px] xl:leading-[95px]`}>EL JUEGO<br/>DE OREO</h1>
+                    <h1 className={`pluto-black text-center tracking-tighter leading-[50px] md:leading-[56px] lg:leading-[75px]`}>EL JUEGO<br/>DE OREO</h1>
                     <button onClick={() => { hideHome() }}
-                        className={`bg-[#F2F1F1] text-[#0054BA] text-2xl lg:text-3xl xl:text-4xl py-3 px-4 xl:py-5 xl:px-5 rounded-md absolute -bottom-12 lg:-bottom-[55px] xl:-bottom-[75px] mx-auto z-50 tracking-tighter pluto-black shadow-[4px_4px_0px_0px_rgba(0,0,0,0.25)] -rotate-6`}>
+                        className={`bg-[#F2F1F1] text-[#0054BA] text-2xl lg:text-3xl py-3 px-4 rounded-md absolute -bottom-12 lg:-bottom-[55px] mx-auto z-50 tracking-tighter pluto-black shadow-[4px_4px_0px_0px_rgba(0,0,0,0.25)] -rotate-6`}>
                         ¡JUGUEMOS!
                     </button>
                 </div>

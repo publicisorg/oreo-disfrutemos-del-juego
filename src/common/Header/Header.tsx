08482a2dcb4b70c214bd812eb93e17a40318f72b
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-07-05 11:49:48 -0300

    correccion desktop

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -1,10 +1,10 @@
 
 export const Header = (props:any) => {
   return (
-    <div className="md:flex md:justify-between md:w-full md:absolute md:top-0">
+    <div className="z-[100] md:flex md:justify-between md:w-full md:absolute md:top-0">
       <img src="./oreo.png" alt="Oreo" className="max-w-[165px] lg:max-w-none lg:w-[180px] xl:w-[200px] md:mt-6 mt-3 md:ml-6 mb-4" />
       <div className="md:mt-10 md:mr-10">
-        <div onClick={() => props.setShowShare(true)} className={`hidden md:flex flex-row justify-center items-center gap-2 md:gap-3 text-[12px] md:text-base duration-300 text-white`}>
+        <div onClick={() => props.setShowShare(true)} className={`cursor-pointer hidden md:flex flex-row justify-center items-center gap-2 md:gap-3 text-[12px] md:text-base duration-300 text-white`}>
           <span>
             <img className="w-4" src="icon-share.svg" />
           </span>
